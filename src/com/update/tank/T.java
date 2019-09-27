277b8d736520432eaa6588549abe73762f88ed88
liupu <575866447@qq.com>
2019-09-28 00:37:39 +0800

    认识 Frame类

--- src/com/update/tank/T.java
+++ src/com/update/tank/T.java
@@ -1,5 +1,10 @@
 package com.update.tank;
 
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 /**
  * @author : liupu
  * date    : 2019/9/28
@@ -7,5 +12,19 @@ package com.update.tank;
  * github : https://github.com/CodeLiuPu/
  */
 public class T {
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+        frame.setSize(800,600);
+        frame.setResizable(false);
+        frame.setTitle("Tank War");
+        frame.setVisible(true);
 
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                System.exit(0);
+            }
+        });
+    }
 }
